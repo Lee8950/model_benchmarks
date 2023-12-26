a5f98d552138c72470f2536a876d902ad28e67db
Lee8950 <13660398950@163.com>
2023-12-26 20:14:11 +0800

    fasternetblock

--- pconv/pconv.py
+++ pconv/pconv.py
@@ -1,14 +1,15 @@
 import tensorflow as tf
+import copy
 
 class PConv2D(tf.keras.layers.Layer):
-    def __init__(self, dim, n_div):
+    def __init__(self, dim, n_div, padding="same"):
         super().__init__()
         self.p_dim = dim // n_div
         self.untouched = dim - self.p_dim
         self.conv2d = tf.keras.layers.Conv2D(self.p_dim,
                                             kernel_size=(3,3),
                                             strides=(1,1),
-                                            padding="same",
+                                            padding=padding,
                                             data_format="channels_last",
                                             )
 
@@ -26,16 +27,32 @@ class MLPBlock(tf.keras.layers.Layer):
         self.first = tf.keras.layers.Conv2D(self.hidden_dim,
                                             kernel_size=(1,1),
                                             strides=(1,1),
-                                            padding="same",
+                                            padding="valid",
                                             data_format="channels_last",
                                             )
         self.activation = tf.keras.layers.ReLU()
         self.second = tf.keras.layers.Conv2D(self.p_dim,
                                             kernel_size=(1,1),
                                             strides=(1,1),
-                                            padding="same",
+                                            padding="valid",
                                             data_format="channels_last",
                                             )
 
     def call(self, inputs):
         return self.second(self.activation(self.first(inputs)))
+    
+class FasternetBlock(tf.keras.layers.Layer):
+    def __init__(self, dim, n_div, hidden_dim, padding="same"):
+        super().__init__()
+        self.hidden_dim = hidden_dim
+        self.p_dim = dim
+        self.first = PConv2D(dim=dim,
+                            n_div=n_div,
+                            padding=padding)
+        
+        self.second = MLPBlock(dim=dim,
+                            hidden_dim=hidden_dim)
+
+    def call(self, inputs):
+        bypass = tf.Tensor.__copy__(inputs)
+        return self.second(self.first(inputs)) + bypass
